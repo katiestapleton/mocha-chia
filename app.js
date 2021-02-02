0b9a0adfac4e2ff1e9c0966a13ce3c92c5179038
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-02-02 10:52:21 -0600

    test- add invidual test

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-module.exports = function(){
+/* module.exports = function(){
     return 'hello';
+}
+*/
+
+module.exports = { 
+    sayHello: function(){
+    return 'hello';
+    },
+    addNumbers: function(value1, value2){
+        return value1 + value2;
+    }
 }
--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,26 @@
 const assert = require('chai').assert;
-const app = require('../app');
+const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('app', function(){
-    it('app should return hello', function(){
-        assert.equal(app(), 'hello');
+    it('sayHello should return hello', function(){
+        let result = sayHello();
+        assert.equal(result, 'hello');
     });
+
+    it('sayHello should return type string', function(){
+        let result = sayHello();
+        assert.typeOf(result, 'string');
+    })
+
+    it('add numbers should be above k', function(){
+        let result = addNumbers(5,5);
+        assert.isAbove(result, 5);
+    })
+
+    it('sayNumbers should return type string', function(){
+        let result = addNumbers();
+        assert.typeOf(result, 'number');
+    })
+
 });
