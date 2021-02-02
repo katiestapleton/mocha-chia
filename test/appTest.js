f6de2636e73cb4ad110232f0480cbebc504850ce
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-02-02 10:54:17 -0600

    app test update. all passed
    shortcut names instead of each function at top

--- test/appTest.js
+++ test/appTest.js
@@ -1,25 +1,26 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+const app = require('../app');
 
 describe('app', function(){
     it('sayHello should return hello', function(){
-        let result = sayHello();
+        let result = app.appsayHello();
         assert.equal(result, 'hello');
     });
 
     it('sayHello should return type string', function(){
-        let result = sayHello();
+        let result = app.sayHello();
         assert.typeOf(result, 'string');
     })
 
     it('add numbers should be above k', function(){
-        let result = addNumbers(5,5);
+        let result = app.addNumbers(5,5);
         assert.isAbove(result, 5);
     })
 
     it('sayNumbers should return type string', function(){
-        let result = addNumbers();
+        let result = app.addNumbers();
         assert.typeOf(result, 'number');
     })
 
