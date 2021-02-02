6df2609f6f6120050cc81c0d298d483a7cda8775
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-02-02 10:58:11 -0600

    shorter version within testing
    updated describes to demonstrate shorter coding

--- test/appTest.js
+++ test/appTest.js
@@ -3,25 +3,29 @@ const assert = require('chai').assert;
 //const addNumbers = require('../app').addNumbers;
 const app = require('../app');
 
+// results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
+
 describe('app', function(){
     it('sayHello should return hello', function(){
-        let result = app.appsayHello();
-        assert.equal(result, 'hello');
+        //let result = app.appsayHello();
+        assert.equal(sayHelloResult, 'hello');
     });
 
     it('sayHello should return type string', function(){
-        let result = app.sayHello();
-        assert.typeOf(result, 'string');
+        //let result = app.sayHello();
+        assert.typeOf(sayHelloResult, 'string');
     })
 
     it('add numbers should be above k', function(){
-        let result = app.addNumbers(5,5);
-        assert.isAbove(result, 5);
+        //let result = app.addNumbers(5,5);
+        assert.isAbove(addNumberResult, 5);
     })
 
     it('sayNumbers should return type string', function(){
-        let result = app.addNumbers();
-        assert.typeOf(result, 'number');
+        //let result = app.addNumbers();
+        assert.typeOf(addNumberResult, 'number');
     })
 
 });
